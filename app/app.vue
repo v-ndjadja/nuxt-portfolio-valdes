63b7c4763dbb921fb9dcb7ee8dcfc4a2615df5bf
VALDES NK <ndjadjavaldes@gmail.com>
2026-02-17 18:13:50 +0100

    travail sur composant

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+<div><h1>RENSEIGNEMENT ME CONCERNANT</h1>
+<mesinfo
+NOM=NDJADJA
+PRENOM=VALDES
+NIVEAU="2 Cycle BTS"
+FILIAIRE="GSI"
+STATUT="Celibataire"
+RESIDENCE="Nkongsamba"
+/>
+<div class= "container">
+<h1 class="display-1 ">
+PORT-FOLIO</h1>
+</div>
+</div>
+</template>
+<script>
+   </script>
+<style scoped>
+</style>
---
+++ app/components/mesinfo.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['NOM','PRENOM','NIVEAU','FILIAIRE','STATUT','RESIDENCE'])
+</script>
+<template>
+<div class="carte-identite"><h3>FICHE D'IDENTITE</h3>
+<p><strong>NOM :</strong>{{NOM}}</p>
+<p><strong>PRENOM :</strong>{{PRENOM}}</p>
+<p><strong>NIVEAU :</strong>{{NIVEAU}}</p>
+<p><strong>FILIAIRE :</strong>{{FILIAIRE}}</p>
+<p><strong>STATUT :</strong>{{STATUT}}</p>
+<p><strong>RESIDENCE :</strong>{{RESIDENCE}}</p>
+</div>
+</template>
